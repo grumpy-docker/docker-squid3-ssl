54d4864e5f936870674a90cab054e1d51e2d3e42
Alex Sherwin <alex.sherwin@acadiasoft.com>
2014-11-13 17:55:23 -0500

    adding echo's to the run.sh file

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 # Prep cache directory
+echo "Prepping cache dir..."
 /usr/sbin/squid3 -z -N -f /mnt/squid3/squid3.conf
 
 # prep log ownership
+echo "Fixing /mnt/log permissions"
 chown -R proxy:proxy /mnt/log
 
 # Run squid
+echo "Starting squid3..."
 /usr/sbin/squid3 -N -f /mnt/squid3/squid3.conf
 
+echo "squid3 has stopped"
