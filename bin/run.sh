5c7563b23f3261ad5830af90b2bff036a54b9e7a
Alex Sherwin <alex.sherwin@acadiasoft.com>
2014-11-13 17:48:01 -0500

    updated to change the log file ownership to proxy:proxy

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,9 @@
 # Prep cache directory
 /usr/sbin/squid3 -z -N -f /mnt/squid3/squid3.conf
 
+# prep log ownership
+chown -R proxy:proxy /mnt/log
+
 # Run squid
 /usr/sbin/squid3 -N -f /mnt/squid3/squid3.conf
 
